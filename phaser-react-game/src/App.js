c5918baf3a29e5866c5862e88ba710791fcad299
ingemar.finn <ingemar.finn@king.com>
2024-06-23 20:12:10 +0200

    Add antdesign start button

--- phaser-react-game/src/App.js
+++ phaser-react-game/src/App.js
@@ -1,13 +1,21 @@
 // src/App.js
-import React from 'react';
+import React, {useState} from 'react';
+import { Button } from 'antd';
 import './App.css';
 import PhaserGame from './components/PhaserGame';
 
 function App() {
+    const [startGame, setStartGame] = useState(false);
+
+    const handleStartGame = () => {
+        setStartGame(true);
+    };
+
   return (
       <div className="App">
         <header className="App-header">
-          <PhaserGame />
+            {!startGame && <Button type="primary" onClick={handleStartGame}>Start</Button>}
+            {startGame && <PhaserGame />}
         </header>
       </div>
   );
